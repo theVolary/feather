7d58c3434d851acf61fa168fb72e7550c821ff97
ryedin <ryan.gahl@gmail.com>
2010-07-05 21:12:11 -0500

    Started process of wrapping main files for dual sided use; started fleshing out some app level API ideas (see /test.js)

--- test.js
+++ test.js
@@ -1,10 +1,50 @@
 var sys = require("sys");
-require("./lib/core"); //adds jojo to the global namespace
-require("./lib/server").init({
-    port: 8089
-});
+var jojo = require("./lib/core").jojo;
 
-jojo.on("test", function() {
-   sys.puts("events are firing..."); 
+var tick;
+var idleTimer;
+
+//TODO: refactor this stuff into modules, and do a middle-ware thing to stack request handlers
+//TODO: make generic configuration file loader for this stuff
+require("./lib/server").init(jojo, {
+    port: 8089,
+    states: {
+        ready: {
+            stateStartup: function(fsm, args) {
+                sys.puts("app is currently waiting on requests");                
+            },
+            request: function(fsm, args) {
+                //we got a new request, move to the "processingRequest" state
+                return fsm.states.processingRequest;
+            }
+        },
+        processingRequest: {
+            stateStartup: function(fsm, args) {
+                tick = (new Date()).getTime();
+                               
+                var req = args.request;
+                var res = args.response;
+                
+                sys.puts("processing request: " + req.url); 
+                
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.write('Custom Request Handler<br/>\n');
+                
+                //test what happens when requests take too long
+                /*setTimeout(function() {
+                    fsm.fire("finishingRequest", args);
+                }, 1000);*/
+               
+                //done processing the request, we can go wait for a new one now
+                fsm.fire("endRequest", args);
+            },
+            endRequest: function(fsm, args) {
+                //simple per-request benchmarking
+                var newTick = (new Date()).getTime();
+                var diff = newTick - tick;
+                sys.puts("request took " + diff + " milliseconds");
+                return fsm.states.ready;
+            }
+        }
+    }
 });
-jojo.fire("test");
