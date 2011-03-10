27654b6fd49cdce0791f3fe1227f61391dcf5a82
skrenek <zeddmaxim@gmail.com>
2011-03-10 16:09:32 -0600

    tabs cleaned up.

--- blog/public/widgets/engine/engine.server.js
+++ blog/public/widgets/engine/engine.server.js
@@ -1,11 +1,11 @@
 jojo.ns("blog");
 
 blog.engine = jojo.widget.create({
-	name: "blog.engine",
-	path: "widgets/engine/",
-	prototype: {
-		initialize: function($super, options) {
-			$super(options);
-		}
-	}		
+  name: "blog.engine",
+  path: "widgets/engine/",
+  prototype: {
+    initialize: function($super, options) {
+      $super(options);
+    }
+  }   
 });
