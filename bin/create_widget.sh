c024379b1afe796310bea84731d54fb0772736b1
skrenek <zeddmaxim@gmail.com>
2011-03-17 00:31:05 -0500

    New!  Run me to create a default widget.

---
+++ bin/create_widget.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+RED=$'\033[1;31m'
+NO_COLOUR=$'\033[0m'
+clear
+echo -n "Which app? "
+read APP
+echo -n "Where do you want your widget inside of public? (e.g. widgets): "
+read LOCATION
+echo $RED "WARNING: This script does NOT check if the widget already exists, and will OVERWRITE if so!" $NO_COLOUR 
+echo -n "Name your widget: "
+read NAME
+
+FULLPATH="${APP}/public/${LOCATION}/${NAME}"
+
+CLIENT_TEMPLATE=$(cat <<EOF
+jojo.ns("${APP}");
+(function() {	
+	${APP}.${NAME} = jojo.widget.create({
+		name: "${APP}.${NAME}",
+		path: "${LOCATION}/${NAME}/",
+		prototype: {
+			initialize: function(\$super, options) {
+				\$super(options);
+			}
+		}		
+	});	
+})();
+EOF
+)
+
+SERVER_TEMPLATE=$(cat <<EOF
+jojo.ns("${APP}");
+${APP}.${NAME} = jojo.widget.create({
+	name: "${APP}.${NAME}",
+	path: "${LOCATION}/${NAME}/",
+	prototype: {
+		initialize: function(\$super, options) {
+			\$super(options);
+		}
+	}		
+});
+
+EOF
+)
+
+mkdir -pv $FULLPATH
+echo "$CLIENT_TEMPLATE" > "${FULLPATH}/${NAME}.client.js"
+echo "$SERVER_TEMPLATE" > "${FULLPATH}/${NAME}.server.js"
+echo "" > "${FULLPATH}/${NAME}.template.html"
+echo -e "Thanks!  Your widget awaits.\a"
