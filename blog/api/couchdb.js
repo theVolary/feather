6913258d2ab6796b10ff70fadfe109879fe8e4a9
skrenek <zeddmaxim@gmail.com>
2011-03-17 00:31:33 -0500

    Completed exportDb.

--- blog/api/couchdb.js
+++ blog/api/couchdb.js
@@ -43,24 +43,169 @@ var couchdb = {
 	 * Queries a view at the connected db.
 	 * 
 	 * @path a string of the form "design_doc_name/view_name"
-	 * @options options object.  Options:
-	 *   <ul>
-	 *   <li>callback: the callback function to call with the result</li>
-	 *   <li>any other options you wish to pass to the view (startKey, endKey, limit, key, descending, etc.)</li>
-	 *   </ul>
+	 * @options options object.  Use this to pass any other options you wish to use when querying the view (startKey, endKey, limit, key, descending, etc.)
 	 */
 	view: function(path, options, callback) {
 	  
 	  return this.db.view(path, options, callback);
 	},
 	
-	exportDb: function(options) {
-	  // export design documents
-	  // export other documents (get all, write to json files)
-	},
+	/**
+	 * Exports all documents from the database to local json files.
+	 * 
+	 * @options an options object.  Options are: dataFolder
+	 * @callback a callback that will be called on completion.
+	 */
+   exportDb: function(options, finalCallback) { 
+     var dataFolder = options.dataFolder || (jojo) ? [jojo.appOptions.appRoot, "data"].join('/') : "./data";
+     var docFolder = [dataFolder, 'docs'].join('/');
+     var designFolder = [dataFolder, 'design'].join('/');
+     var fs = require("fs");
+     var path = require("path");
+     var sys = require("sys");
+     
+     var exportFsm = new jojo.fsm.finiteStateMachine({
+        states: {
+          initial: {
+            stateStartup: function(fsm, args) {
+              sys.puts("state initial");
+            },
+            verifyPaths: function(fsm, args) {
+              sys.puts("in verifyPaths");
+              return fsm.states.verifyingDataFolder;
+            }
+          },            
+          verifyingDataFolder: {
+            stateStartup: function(fsm, args) {
+              sys.puts("starting to verify data folder");
+              path.exists(dataFolder, function(exists) {
+                if (!exists) {
+                  fsm.fire('createFolder', args);
+                } else {
+                  fsm.fire('pathVerified', args);
+                }
+              });
+            },
+            createFolder: function(fsm, args) {
+              sys.puts("vdf.createFolder");
+              fs.mkdir(dataFolder, '0755', function(err) {
+                if (err) { fsm.fire('error', { err: err }); return; }
+                fsm.fire('pathVerified', args);
+              })
+            },
+            pathVerified: function(fsm, args) {
+              sys.puts("vdf.pathVerified");
+              return fsm.states.verifyingSubFolders;
+            },
+            error: function(fsm, args) {
+              sys.puts("vdf.error");
+              return fsm.states.aborted;
+            }
+          }, // end verifyingDataFolder states
+          verifyingSubFolders: {
+            stateStartup: function(fsm, args) {
+              sys.puts("state verifyingSubFolders")
+              fsm.subpathSemaphore = new jojo.lang.semaphore(function() {
+                return fsm.states.exporting;
+              });
+              fsm.fire('verifyFolder', { path: designFolder });
+              fsm.fire('verifyFolder', { path: docFolder });
+            },
+            verifyFolder: function(fsm, args) {
+              sys.puts("vsf.verifyFolder");
+              fsm.subpathSemaphore.increment();
+              path.exists(args.path, function(exists){
+                if (!exists) {
+                  fsm.fire('createFolder', args);
+                } else {
+                  fsm.fire('pathVerified', args);
+                }
+              });
+            }, 
+            createFolder: function(fsm, args) {
+              sys.puts("vsf.createFolder");
+              fs.mkdir(args.path, '0755', function(err) {
+                if (err) fsm.fire('error', { err: err });
+                fsm.fire('pathVerified', args);
+              })
+            },
+            pathVerified: function(fsm, args) {
+              sys.puts("vsf.pathVerified");
+              return fsm.subpathSemaphore.execute();
+            },
+            error: function(fsm, args) {
+              sys.puts("vsf.error");
+              return fsm.states.aborted;
+            }
+          },
+          aborted: {
+            stateStartup: function(fsm, args) {
+              sys.puts("state aborted");
+              if (finalCallback) finalCallback(args.err);
+            }
+          },
+          exporting: {
+            stateStartup: function(fsm, args) {
+              sys.puts("state exporting");
+              delete fsm.subpathSemaphore;
+              couchdb.db.all({ include_docs:true }, function(err, res) {
+                if (err) {
+                  fsm.fire('error', { err: err });
+                } else { // Go speed racer!
+                  fsm.docSemaphore = new jojo.lang.semaphore(function() {
+                    fsm.fire("completing", args);
+                  })
+                  res.forEach(function(key, value, id) {
+                    fsm.fire('processDocument', { doc: value });
+                  });
+                }
+              });
+            }, // end stateStartup
+            processDocument: function(fsm, args) {
+              sys.puts("exp.processDocument");
+              fsm.docSemaphore.increment();
+              var doc = args.doc;
+              var folder = docFolder;
+              var filename = doc._id;
+
+              // Strip off the rev num.
+              if (doc.rev) delete doc.rev;
+              if (doc._rev) delete doc._rev;
+              
+              // Determine folder (design docs go in design)
+              if (doc._id.indexOf('_design/') === 0) {
+                folder = designFolder;
+                filename = doc._id.substring(8);
+              }
+              fs.writeFile([folder, filename].join('/') + '.json', JSON.stringify(doc), "utf8", function(err) {
+                sys.puts("writeFile callback.  semaphore is " + fsm.docSemaphore.semaphore);
+                if (err) { fsm.fire('error', { err: err }); return; }
+                return fsm.docSemaphore.execute();                
+              });
+            }, // end processDocument event
+            error: function(fsm, args) {
+              sys.puts("exp.error");
+              return fsm.states.aborted;
+            },
+            completing: function(fsm, args) {
+              return fsm.states.complete;
+            }
+          }, // end exporting state
+          complete: {
+            stateStartup: function(fsm, args) {
+              sys.puts("state complete");
+              delete fsm.docSemaphore;
+              if (finalCallback) finalCallback(null);
+            }
+          } // end complete state
+        } // end states
+      }); // end fsm
+      
+      exportFsm.fire('verifyPaths');
+   },
 	
 	exportView: function(path, options) {
-	  var dataFolder = "/data";
+	  var dataFolder = options.dataFolder || "/data";
 	  
 	},
 	
