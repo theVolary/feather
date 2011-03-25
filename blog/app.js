f1c2934628ef799ec61e8065cf67ae551d757de7
ryedin <ryan.gahl@gmail.com>
2011-03-25 15:42:51 -0500

    minor formatting and config changes to blog/app.js

--- blog/app.js
+++ blog/app.js
@@ -29,7 +29,7 @@ jojo.init({
       appdb: {
         hostUrl: 'http://localhost',
         dbName:'jojoblog',
-        auth: { username:'jojoadmin', password:'password'}
+        auth: { username:'jojoadmin', password:'jojoadmin'}
       }
       
     },
@@ -50,26 +50,29 @@ jojo.init({
         },
         { 
           type: 'file', 
-          disabled: false, 
+          disabled: true, 
           options: { 
             layout: 'colored', 
             filename: 'jojoblog.log', 
             maxFileSize:10*1024*1024, 
             numBackups:2, 
             filePollInterval:60, 
-            includedCategories: ['jojo.http'] },
+            includedCategories: ['jojo.http'],
             levelThreshold: 'info'
-          },
+          }
+        },
         { 
           type: 'file', 
-          disabled: false, 
+          disabled: true, 
           options: { 
             layout: 'colored', 
             filename: 'nonhttp.log', 
             maxFileSize:10*1024*1024, 
             numBackups:2, 
             filePollInterval:60, 
-            excludedCategories: ['jojo.http'] } },
+            excludedCategories: ['jojo.http'] 
+          } 
+        },
         { 
           type: 'url', 
           disabled: true, 
@@ -118,50 +121,47 @@ jojo.init({
         
     ],
     states: {
-        ready: {
-            stateStartup: function(fsm, args) {
-              jojo.logger.info({message:"app is currently waiting on requests", category:'jojo.http'});
-            },
-            request: function(fsm, args) {
-                //we got a new request, move to the "processingRequest" state
-                return fsm.states.processingRequest;
-            }
+      ready: {
+        stateStartup: function(fsm, args) {
+          jojo.logger.info({message:"app is currently waiting on requests", category:'jojo.http'});
         },
-        processingRequest: {
-            stateStartup: function(fsm, args) {
-                var req = args.request;
-                var res = args.response;
-                
-                //basic benchmarking
-                req.startTime = (new Date()).getTime();
-                
-                jojo.logger.trace({templateId:'separator', category:'jojo.http'});
-                jojo.logger.trace({message:"processing request: ${url}", replacements:req, category:'jojo.http'});
-                                
-                req.on("end", function() {
-                    fsm.fire("endRequest", {request: req, response: res});
-                });     
-                
-                //start running through the middleware
-                args.next();
-                
-                //go back and wait for the next request
-                return fsm.states.ready;               
-            }
-        },
-        global: {
-          endRequest: function(fsm, args) {
-            //done processing the request, we can go wait for a new one now
-            //simple per-request benchmarking
-            var req = args.request;
-            var newTick = (new Date()).getTime();
-            var diff = newTick - req.startTime;
-            if (jojo.logger) {
-                jojo.logger.trace({message:"request took ${ms} milliseconds", replacements:{ms:diff}, category:'jojo.http'});
-                jojo.logger.trace({templateId:'separator', category:'jojo.http'});
-//                jojo.logger.flush();
-            }
-          }
+        request: function(fsm, args) {
+          //we got a new request, move to the "processingRequest" state
+          return fsm.states.processingRequest;
         }
+      },
+      processingRequest: {
+        stateStartup: function(fsm, args) {
+          var req = args.request;
+          var res = args.response;
+          
+          //basic benchmarking
+          req.startTime = (new Date()).getTime();
+          
+          jojo.logger.trace({templateId:'separator', category:'jojo.http'});
+          jojo.logger.trace({message:"processing request: ${url}", replacements:req, category:'jojo.http'});
+                          
+          req.on("end", function() {
+            fsm.fire("endRequest", {request: req, response: res});
+          });     
+          
+          //start running through the middleware
+          args.next();
+          
+          //go back and wait for the next request
+          return fsm.states.ready;               
+        }
+      },
+      global: {
+        endRequest: function(fsm, args) {
+          //done processing the request, we can go wait for a new one now
+          //simple per-request benchmarking
+          var req = args.request;
+          var newTick = (new Date()).getTime();
+          var diff = newTick - req.startTime;
+          jojo.logger.trace({message:"request took ${ms} milliseconds", replacements:{ms:diff}, category:'jojo.http'});
+          jojo.logger.trace({templateId:'separator', category:'jojo.http'});
+        }
+      }
     }
 });
