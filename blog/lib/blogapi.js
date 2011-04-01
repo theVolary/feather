49954bf345348ef4e52a2776f5aafd5b0534f592
skrenek <zeddmaxim@gmail.com>
2011-04-01 09:38:25 -0500

    Cleanup to work with testing.

--- blog/lib/blogapi.js
+++ blog/lib/blogapi.js
@@ -6,17 +6,20 @@ exports.BlogApi = Class.create({
     jojo.logger.info('Getting posts by date from couch.');
     jojo.data.appdb.view("blogentry/posts_by_date", { descending: true }, function(err, dbResult) {
       if (!err) {
-        jojo.logger.info({message:'Found ${cnt} posts.', replacements:{cnt:dbResult.length}});
+        jojo.logger.info({message:'Found ' + dbResult.length + ' posts.'});
         dbResult.forEach(function(key, doc, id) {
+          debugger;
           doc.key = key;
-          doc.pubDate = new Date(key[0], key[1], key[2], key[3], key[4], key[5]);
+          doc.pubDate = new Date(key[0]-0, key[1]-0, key[2]-0, key[3]-0, key[4]-0, key[5]-0);
           doc.id = id;
-          jojo.logger.debug({message:'Found document w/ id ${id}, key ${key}', replacements:doc});
+          jojo.logger.debug({message:'Found document w/ id ' + id + ', key ' + key});
         });
       } else {
         jojo.logger.error(err);
       }
-      callback(err, dbResult);
+      if (callback) {
+        callback(err, dbResult);
+      }
     }); // end couch.db.view
   }
   
