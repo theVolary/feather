4570cbf345fdab7a57581cdba682820dccec69cf
ryedin <ryan.gahl@gmail.com>
2011-04-19 14:33:15 -0500

    added optimized jQuery.template usage wherever possible (i.e. wherever an actual DOM appendTo was not required)

---
+++ performance_app/public/widgets/async_1/async_1.client.js
@@ -0,0 +1,12 @@
+feather.ns("performance_app");
+(function() {	
+	performance_app.async_1 = feather.widget.create({
+		name: "performance_app.async_1",
+		path: "widgets/async_1/",
+		prototype: {
+			initialize: function($super, options) {
+				$super(options);
+			}
+		}		
+	});	
+})();
---
+++ performance_app/public/widgets/no_async_1/no_async_1.client.js
@@ -0,0 +1,12 @@
+feather.ns("performance_app");
+(function() {	
+	performance_app.no_async_1 = feather.widget.create({
+		name: "performance_app.no_async_1",
+		path: "widgets/no_async_1/",
+		prototype: {
+			initialize: function($super, options) {
+				$super(options);
+			}
+		}		
+	});	
+})();
