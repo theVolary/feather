3415ef6734119c4e58d798c18eb121d1f0272585
skrenek <zeddmaxim@gmail.com>
2011-04-21 12:51:57 -0500

    Documentation is done, pending the discovery of any omissions / errors.

--- featherdoc/bin/gendoc.sh
+++ featherdoc/bin/gendoc.sh
@@ -13,6 +13,7 @@ else
     exit 1
   fi
   
-  node lib/node-jsdoc-toolkit/app/run.js --exclude="date\.js$" --exclude="jquery.*\.js$" --exclude=".*test.*" --exclude="socket\.io\.client" --exclude="node-htmlparser" --verbose --multiple --recurse  --directory=public/docs/api --template=lib/node-jsdoc-toolkit/templates/codeview ../lib/node-core-enhancements ../lib  
+  node lib/node-jsdoc-toolkit/app/run.js --exclude="date\.js$" --exclude="jquery.*\.js$" --exclude=".*test.*" --exclude="socket\.io\.client" --exclude="node-htmlparser" --exclude="feather-client" --verbose --multiple --recurse  --directory=public/docs/api/server --template=lib/node-jsdoc-toolkit/templates/codeview ../lib/node-core-enhancements ../lib  
 
+  node lib/node-jsdoc-toolkit/app/run.js --exclude="jquery.*\.js$" --exclude=".*test.*" --exclude="sha512" --exclude="json2" --verbose --multiple --recurse  --directory=public/docs/api/client --template=lib/node-jsdoc-toolkit/templates/codeview ../lib/feather-client
 fi
--- featherdoc/public/w/docnav/docnav.server.js
+++ featherdoc/public/w/docnav/docnav.server.js
@@ -7,11 +7,12 @@ featherdoc.navItems = [
   {type:'markdown', name:'Auth', url:'https://github.com/skrenek/jojojs/raw/master/docs/auth.md'},
   {type:'markdown', name:'Logging', url:'https://github.com/skrenek/jojojs/raw/master/docs/logging.md'},
   {type:'markdown', name:'Testing Apps', url:'https://github.com/skrenek/jojojs/raw/master/docs/testing.md'},
-  {type:'api', name:'API Docs', url:'/docs/api/index.html'}
+  {type:'api', name:'Server API', url:'/docs/api/server/index.html'},
+  {type:'api', name:'Client API', url:'/docs/api/client/index.html'}
 ];
 
 featherdoc.getNavItems = function(cb) {
-  cb && cb({navItems: featherdoc.navItems });
+  cb && cb(null, {navItems: featherdoc.navItems });
 };
 
 featherdoc.docnav = feather.widget.create({
