05aba51d88553ed6a518fd4e0d671db5804bd39a
skrenek <zeddmaxim@gmail.com>
2011-05-12 09:33:22 -0500

    Try to add vars to .bashrc first, then .profile.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 echo -n "Where is your feather installation? [`pwd`]"
-read FINSTALL
+read FEATHER_HOME
 
-if [ "$FINSTALL" = "" ]; then
-  FINSTALL=`pwd`
+if [ "$FEATHER_HOME" = "" ]; then
+  FEATHER_HOME=`pwd`
 fi
 
-echo "" >> ~/.profile
-echo "# Feather Vars" >> ~/.profile 
-echo "export FEATHER_HOME=$FINSTALL" >> ~/.profile
-echo "export PATH=$FEATHER_HOME/bin:$PATH" >> ~/.profile
-export FEATHER_HOME=$FINSTALL
+# Preferably append to .bashrc.  Most systems that use .bashrc typically don't use .profile by default.
+
+if [ -f "$HOME/.bashrc" ]; then
+  echo "" >> ~/.bashrc
+  echo "# Feather Vars" >> ~/.bashrc
+  echo "export FEATHER_HOME=$FEATHER_HOME" >> ~/.bashrc
+  echo "export PATH=$FEATHER_HOME/bin:$PATH" >> ~/.bashrc
+else
+  echo "" >> ~/.profile
+  echo "# Feather Vars" >> ~/.profile 
+  echo "export FEATHER_HOME=$FEATHER_HOME" >> ~/.profile
+  echo "export PATH=$FEATHER_HOME/bin:$PATH" >> ~/.profile
+fi
+export FEATHER_HOME
 export PATH=$FEATHER_HOME/bin:$PATH
+echo "Setup complete."
