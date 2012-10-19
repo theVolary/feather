30b6e242e8a75402de9df202d6031cb809f987db
ryedin <ryan.gahl@gmail.com>
2012-10-19 11:34:03 -0500

    datalinking fixes

--- api_tester/public/widgets/datalinkOptionsTest/datalinkOptionsTest.client.js
+++ api_tester/public/widgets/datalinkOptionsTest/datalinkOptionsTest.client.js
@@ -35,7 +35,7 @@ feather.ns("api_tester");
                   }
                 },
                 containerOptions: {
-                  title: "Enter text and then click 'DONE' button",
+                  title: "Enter text in the textbox and then click 'DONE' button",
                   width: 500,
                   height: 300,
                   buttons: {
@@ -53,13 +53,121 @@ feather.ns("api_tester");
         }));
 
         suite.add(new Y.Test.Case({
-          name: "Test boolean/checkbox fields when model is known at render time",
+          name: "Test checkbox changes to model",
           setUp: feather.emptyFn,
           tearDown : function(){
             me.datalink1 && me.datalink1.dispose();
           },
 
-          testCheckboxes: function () {
+          testCheckboxClickChangesModel: function () {
+            var test = this;
+            feather.Widget.load({
+              path: "widgets/datalink1/",
+              parent: me,
+              clientOptions: {
+                on: {
+                  ready: function(sender) {
+                    me.datalink1 = sender;
+                  }
+                },
+                containerOptions: {
+                  title: "Check the checkbox and then click 'DONE' button",
+                  width: 500,
+                  height: 300,
+                  buttons: {
+                    DONE: function() {
+                      test.resume(function() {
+                        Y.Assert.areEqual(me.datalink1.model.test.bool, true);                        
+                      });
+                    }
+                  }
+                }
+              }
+            });
+            test.wait();
+          }
+        }));
+
+        suite.add(new Y.Test.Case({
+          name: "Test radio changes to model",
+          setUp: feather.emptyFn,
+          tearDown : function(){
+            me.datalink1 && me.datalink1.dispose();
+          },
+
+          testRadioClickChangesModel: function () {
+            var test = this;
+            feather.Widget.load({
+              path: "widgets/datalink1/",
+              parent: me,
+              clientOptions: {
+                on: {
+                  ready: function(sender) {
+                    me.datalink1 = sender;
+                  }
+                },
+                containerOptions: {
+                  title: "Check the radio for '1' and then click 'DONE' button",
+                  width: 500,
+                  height: 300,
+                  buttons: {
+                    DONE: function() {
+                      test.resume(function() {
+                        Y.Assert.areEqual(me.datalink1.model.test.radio, '1');                        
+                      });
+                    }
+                  }
+                }
+              }
+            });
+            test.wait();
+          }
+        }));
+
+        suite.add(new Y.Test.Case({
+          name: "Test select changes to model",
+          setUp: feather.emptyFn,
+          tearDown : function(){
+            me.datalink1 && me.datalink1.dispose();
+          },
+
+          testSelectChangesModel: function () {
+            var test = this;
+            feather.Widget.load({
+              path: "widgets/datalink1/",
+              parent: me,
+              clientOptions: {
+                on: {
+                  ready: function(sender) {
+                    me.datalink1 = sender;
+                  }
+                },
+                containerOptions: {
+                  title: "Choose '2' from the select dropdown and then click 'DONE' button",
+                  width: 500,
+                  height: 300,
+                  buttons: {
+                    DONE: function() {
+                      test.resume(function() {
+                        Y.Assert.areEqual(me.datalink1.model.test.select, '2');                        
+                      });
+                    }
+                  }
+                }
+              }
+            });
+            test.wait();
+          }
+        }));
+
+        suite.add(new Y.Test.Case({
+          name: "Test fields when model is known at render time",
+          setUp: feather.emptyFn,
+          tearDown : function(){
+            me.datalink1 && me.datalink1.dispose();
+          },
+
+          testFieldsAtRenderTime: function () {
             var test = this;
             feather.Widget.load({
               path: "widgets/datalink1/",
@@ -69,17 +177,27 @@ feather.ns("api_tester");
                   ready: function() {
                     me.datalink1 = this;
                     test.resume(function() {
-                      Y.Assert.areEqual(me.datalink1.get('[name=bool]').attr('checked'), 'checked');                        
+                      //test the textbox
+                      Y.Assert.areEqual('bar', me.datalink1.get('[name=foo]').val()); 
+                      //test the checkbox
+                      Y.Assert.areEqual('checked', me.datalink1.get('[name=bool]').attr('checked'));    
+                      //test the radio
+                      Y.Assert.areEqual('checked', me.datalink1.get('#radio1').attr('checked'));      
+                      //test the select
+                      Y.Assert.areEqual('2', me.datalink1.get('select').val());                        
                     });
                   }
                 },
                 model: {
                   test: {
-                    bool: true
+                    foo: 'bar',
+                    bool: true,
+                    radio: '1',
+                    select: '2'
                   }
                 },
                 containerOptions: {
-                  title: "testing checkbox fields",
+                  title: "testing fields",
                   width: 500,
                   height: 300
                 }
